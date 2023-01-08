22f92186c994e02af4daf1577c6429a436516988
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-01-07 22:29:06 -0600

    Create 03_Substring.java

---
+++ exercises/code/Strings/03_Substring.java
@@ -0,0 +1,27 @@
+/**
+  Request an String and reverse it and return it.
+  
+  You need to request an String and the you need to return that String but in the reverse order
+  
+  for example "circus" must be returned as "sucric"
+*/
+class 03_ReverseString {
+  
+  public static void main(String[] args){
+   
+    BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+ 
+        // Reading data using readLine
+        String s = reader.readLine();
+    
+    System.out.println(reverseString(s));
+    
+  }
+  
+  public static String reverseString(String s){
+   
+    //Add your code here
+  }
+
+}
