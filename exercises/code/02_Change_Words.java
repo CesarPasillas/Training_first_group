a0f2cdcf182b36007dae097a90f259f65de17da9
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-01-07 22:13:04 -0600

    Create 02_Change_Words.java

---
+++ exercises/code/02_Change_Words.java
@@ -0,0 +1,21 @@
+/**
+Use the next String: "Wicca la religión de las brujas, es lo nos dicen cuándo preguntamos o buscamos sobre Wicca. Aunque tal vez escuchaste la palabra Wicca, pero no sabes qué es o a qué se refiere"
+ 
+and change all the lower 'a' for the capital 'A'
+*/
+
+class 02_Change_Words {
+  
+  public static void main(String[] args) {
+  
+  String phrase = "Wicca la religión de las brujas, es lo nos dicen cuándo preguntamos o buscamos sobre Wicca. Aunque tal vez escuchaste la palabra Wicca, pero no sabes qué es o a qué se refiere";
+    
+  System.out.println(changeWord(phrase));
+    
+  }
+  
+  public static void changeWord(String s){
+    //Add your code here
+  }
+  
+}
