7cbf3a92f27170508e3a38ebca43d546f36c7f6b
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-01-07 22:48:24 -0600

    Create 05_TwoStrings.java

---
+++ exercises/code/Strings/05_TwoStrings.java
@@ -0,0 +1,24 @@
+/**Determine if two Strings has a substring in common
+ example: hello and world  has two substring "o" and "l" so they ar common and return YES
+  example: hi and world  doesn't has any substring they return NO
+*/
+
+class 05_TwoStrings {
+  
+  public static void main(String [] args){
+    
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    
+    
+    String s1 = bufferedReader.readline();
+    String s2 = bufferedReader.readline();
+    
+    System.out.println(twoStrings(s1, s2));
+    
+  }
+  
+  public static String twoStrings(String s1, String s2) {
+    // Write your code here
+
+    }
+}
