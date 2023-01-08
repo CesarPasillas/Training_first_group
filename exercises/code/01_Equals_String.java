d056b2ea8ba4bf9aa4ab012740ca82336465ba25
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-01-07 22:07:11 -0600

    Create 01_Equals_String.java

---
+++ exercises/code/01_Equals_String.java
@@ -0,0 +1,27 @@
+/**
+ Compare three String and idicate if the three are equal or not.
+ 
+ The mehot should print
+ "Strings first and second are equal: true"
+ "Strings first and second are equal: false"
+ 
+ Depending of the result of the comparing
+*/
+
+class 01_Equals_String {
+  
+  public static void main(String[] args) {
+
+    // create 3 strings
+    String first = "java programming";
+    String second = "java programming";
+    String third = "python programming";
+
+    
+  }
+  
+  public static void compareStrings(String first, String second, String third){
+    //Add your code here
+  }
+  
+}
